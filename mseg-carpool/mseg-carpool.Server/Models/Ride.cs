5fa182bf1fae0b532ce49d72897567f5c15e72dc
USER_NAME <USER_EMAIL>
2024-07-17 12:05:51 +0300

    backend setup ahmed done

---
+++ mseg-carpool/mseg-carpool.Server/Models/Ride.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mseg_carpool.Server.Models
+{
+
+
+    public class Ride
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int AvailableSeats { get; set; }
+        public DateTime DepartureTime { get; set; }
+
+        public int? Driver { get; set; }
+        public User User { get; set; }
+    }
+}
